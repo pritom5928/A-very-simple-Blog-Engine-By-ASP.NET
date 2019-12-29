f191f9d18a1b3b11ba8716cd869311951a22f6f6
pri.tom78 <pritompal789@gmail.com>
2019-12-29 21:03:47 +0600

    myfirstaspproject

---
+++ BlogEngine/Controllers/BlogController.cs
@@ -0,0 +1,43 @@
+﻿using BlogEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogEngine.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Blog blog)
+        {
+            if (ModelState.IsValid)
+            {
+                var ctx = new ApplicationDbContext();
+
+                ctx.Blogs.Add(blog);
+                ctx.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public ActionResult Generic()
+        {
+            return View();
+        }
+    }
+}
---
+++ BlogEngine/Models/Blog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BlogEngine.Models
+{
+    public class Blog
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required, StringLength(50)]
+        public string Title { get; set; }
+
+        [Required, StringLength(50)]
+        public string Author { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+    }
+}
